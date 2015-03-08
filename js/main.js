d3a931efcd53c4233ceb4de674f8a2b6daaed79f
shelleybabij <babijs2@hotmail.com>
2015-03-08 12:01:06 -0400

    feat(html): add smooth scroll to down arrow

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-var $win =$(window);
-
-
-$win.on('scroll', function () {
-	var scrollPos = $win.scrollTop();
-	var newY = (scrollPos);
-
-
-$('.icon-down-arrow').localScroll();
+$('.smooth-scroll').localScroll();
