c7e1b651d3fcb0df6bfa9e6fc593bb3fb5584c76
shelleybabij <babijs2@hotmail.com>
2015-03-09 15:39:25 -0400

    fix(js): delay counter animation on homepage

--- js/main.js
+++ js/main.js
@@ -3,9 +3,37 @@ $('.smooth-scroll').localScroll();
 var $counter = $('.counter');
 
 $counter.waypoint(function () {
-	console.log ('waypoint'); },
+  $(".counter").addClass("animate");
+  animateNow();
+  },
+  {
+    offset:400
+  }
+);
 
-	{offset: '75%'});
+function animateNow(){
 
+	$('.animate').each(function() {
+	  var $this = $(this),
+	      countTo = $this.attr('data-count');
 
+	  $({ countNum: $this.text()}).animate({
+	    countNum: countTo
+	  },
 
+	  {
+	    duration: 1000,
+	    easing:'linear',
+
+	    step: function() {
+	      $this.text(Math.floor(this.countNum));
+	    },
+	    complete: function() {
+	      $this.text(this.countNum);
+	      //alert('finished');
+	    }
+
+	  });
+
+	});
+}
